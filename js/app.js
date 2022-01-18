56a6b91408365b8a58b6aea70dc6a6b76e5a69e4
yablko <yablko@brm.sk>
2022-01-18 13:18:46 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+document.getElementById("demo").innerHTML = `
+	Screen Width: ${screen.width}, Screen Height: ${screen.height} <br>
+	Inner Width: ${window.innerWidth}, Inner Height: ${window.innerHeight} <br>
+	Client Width: ${document.documentElement.clientWidth}, Client Height: ${document.documentElement.clientHeight}
+`;
+
 // DATA
 const maxWordLength = 5;
 const maxTries = 6;
